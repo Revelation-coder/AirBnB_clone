a8c1001a832c16e553a7fda12d50a94b9f199ace
Revelation-coder <captainrevelation@gmail.com>
2023-05-10 06:48:49 -0700

    done with initial version

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Amenity class a child of BaseModel"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Our Amenity class with a string attribute"""
+    name = ""
