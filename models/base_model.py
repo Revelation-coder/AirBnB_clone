6485227d05668d63cecdf86c52d1bf178dc8d451
Revelation-coder <captainrevelation@gmail.com>
2023-05-14 04:28:29 -0700

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """ Base  model class that defines all common attributes/methods"""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -22,7 +21,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """Returns a  user friendly representationof the instance"""
@@ -32,7 +30,6 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute with current date&time"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary with  all instance __dict__ keys/values"""
