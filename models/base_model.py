a5a42f9469474c35a0bfc8837ad7394eec6214a6
Revelation-coder <captainrevelation@gmail.com>
2023-05-14 04:45:50 -0700

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """ Base  model class that defines all common attributes/methods"""
 import uuid
 from datetime import datetime
-from models import storage
 
 class BaseModel:
     """The main base class"""
@@ -21,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+
     def __str__(self):
         """Returns a  user friendly representationof the instance"""
         return ("[{}] ({}) {}".format(self.__class__.__name__,
